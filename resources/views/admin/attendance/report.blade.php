131801d502dc92007d3a8b9e28c374b12ab474b6
Mohona <mohona7735@gmail.com>
2023-08-19 01:50:02 +0600

    salary merge

--- resources/views/admin/attendance/report.blade.php
+++ resources/views/admin/attendance/report.blade.php
@@ -51,7 +51,78 @@
             <tbody>
 
 
+              {{-- @php
+    $successCount = 0; // Initialize the success count variable
+@endphp
 
+@foreach ($employees as $employee)
+    <input type="hidden" name="employee_id" value="{{ $employee->id }}">
+    <tr>
+        <td>{{ $employee->firstname }} {{ $employee->lastname }}</td>
+                        <td>{{ $employee->designation->title }}</td>
+                        <td>{{ $employee->id }}</td>
+        @for ($i = 1; $i < $today->daysInMonth + 1; ++$i)
+            @php
+                                
+                                $date_picker = \Carbon\Carbon::createFromDate($today->year, $today->month, $i)->format('Y-m-d');
+                                
+                                $check_attd = \App\Models\Attendance::query()
+                                    ->where('employee_id', $employee->id)
+                                    ->where('attendance_date', $date_picker)
+                                    ->first();
+                                
+                                $check_depart = \App\Models\Depart::query()
+                                
+                                    ->where('employee_id', $employee->id)
+                                    ->where('depart_date', $date_picker)
+                                    ->first();
+                                
+                            @endphp
+            <td>
+                <div class="form-check form-check-inline">
+                    @if (isset($check_attd))
+                        @if ($check_attd->status == 1)
+                            <i class="fa fa-check text-success"></i>
+                            @php
+                                $successCount++; // Increment the success count
+                            @endphp
+                        @else
+                            <i class="fa fa-check text-danger"></i>
+                        @endif
+                    @else
+                        <i class="fas fa-times text-danger"></i>
+                    @endif
+                </div>
+                <div class="form-check form-check-inline">
+                                  
+                                    @if (isset($check_depart))
+                                    @if ($check_depart->status==1)
+                                    <i class="fa fa-check text-success"></i>
+                                    @else
+                                    <i class="fa fa-check text-danger"></i>
+                                    @endif
+                                  
+                               @else
+                               <i class="fas fa-times text-danger"></i>
+                               @endif
+                                
+
+                                </div>
+            </td>
+            <!-- ... Your existing code ... -->
+        @endfor
+    </tr>
+    <!-- ... Your existing code ... -->
+@endforeach
+
+@php
+    echo "Number of successes: " . $successCount; // Display the success count
+@endphp --}}
+
+
+@php
+    $successIcons = collect(); // Initialize a collection to store success icons
+@endphp
 
 
                 @foreach ($employees as $employee)
@@ -93,7 +164,7 @@
 
                                     @if (isset($check_attd))
                                          @if ($check_attd->status==1)
-                                         <i class="fa fa-check text-success"></i>
+                                         <i class="fa fa-check text-success present"></i>
                                          @else
                                          <i class="fa fa-check text-danger"></i>
                                          @endif
@@ -106,7 +177,7 @@
                                   
                                     @if (isset($check_depart))
                                     @if ($check_depart->status==1)
-                                    <i class="fa fa-check text-success"></i>
+                                    <i class="fa fa-check text-success present"></i>
                                     @else
                                     <i class="fa fa-check text-danger"></i>
                                     @endif
@@ -124,7 +195,10 @@
                     </tr>
                 @endforeach
 
-
+@php
+    $successCount = $successIcons->count();
+    echo "Number of success icons: " . $successCount;
+@endphp
 
 
 
@@ -143,5 +217,16 @@
 @endsection
 
 @section('script')
-
+<script>
+    $(document).ready(function() {
+      // var present = 0;
+      // $('.present').each(function() {
+      //   console.log(present++);
+      // });
+      var successCount = $(".fa.fa-check.text-success").length;
+        
+        // Display the count in the browser's console
+        console.log("Number of success icons: " + successCount);
+    });
+</script>
 @endsection
